fdd7bc9a339ab8744c5026793354f550a4828b8f
shruti <shrutipandit273@gmail.com>
2024-09-27 21:33:14 +0530

    updates server file

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ require('dotenv').config();
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('This is Home Page');
 });
 
+app.get('/about', (req, res) => {
+    res.send('This is About Page');
+});
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
